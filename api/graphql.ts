16f8278ed55de5102006a133b4f91bc2a6490a70
seagalputra <seagalputra@gmail.com>
2021-04-14 22:06:57 +0700

    Remove sdl file and config file

--- api/graphql.ts
+++ api/graphql.ts
@@ -1,21 +1,20 @@
-import "reflect-metadata";
-import micro from "micro-cors";
-import { ApolloServer } from "apollo-server-micro";
-import { buildSchemaSync } from "type-graphql";
-import { GraphQLSchema } from "graphql";
-import { connect, Mongoose } from "mongoose";
+import 'reflect-metadata';
+import micro from 'micro-cors';
+import { ApolloServer } from 'apollo-server-micro';
+import { buildSchemaSync } from 'type-graphql';
+import { GraphQLSchema } from 'graphql';
+import { connect, Mongoose } from 'mongoose';
 
-import { TransactionResolver } from "./transaction/transaction.resolver";
+import { TransactionResolver } from './transaction/transaction.resolver';
 
 const schema: GraphQLSchema = buildSchemaSync({
   resolvers: [TransactionResolver],
-  emitSchemaFile: true,
   validate: false,
 });
 const cors = micro();
 
 (async () => {
-  const mongoDb: string = "mongodb://localhost:27017/silver";
+  const mongoDb: string = process.env.DATABASE_URL!;
   const mongoose: Mongoose = await connect(mongoDb, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -24,8 +23,8 @@ const cors = micro();
 })();
 
 const server = new ApolloServer({ schema });
-const handler = server.createHandler({ path: "/api/graphql" });
+const handler = server.createHandler({ path: '/api/graphql' });
 
 export default cors((req, res) =>
-  req.method === "OPTIONS" ? res.end() : handler(req, res)
+  req.method === 'OPTIONS' ? res.end() : handler(req, res)
 );
