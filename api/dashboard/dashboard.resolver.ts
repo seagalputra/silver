2f1daa352d6529bac0e3dcced8d7061b797546c7
seagalputra <seagalputra@gmail.com>
2021-04-17 20:59:51 +0700

    Adding resolver for dashboard

--- api/dashboard/dashboard.resolver.ts
+++ api/dashboard/dashboard.resolver.ts
@@ -1,15 +1,39 @@
 import { Query, Resolver } from 'type-graphql';
 import { Dashboard } from './dashboard.schema';
+import { TransactionModel } from '../transaction/transaction.schema';
 
 @Resolver((of) => Dashboard)
 export class DashboardResolver {
   @Query((returns) => Dashboard)
-  dashboard(): Dashboard {
+  async dashboard(): Promise<Dashboard> {
+    const result = await TransactionModel.aggregate([
+      {
+        $lookup: {
+          from: 'transactiondetails',
+          localField: 'transactionDetail',
+          foreignField: '_id',
+          as: 'transactionDetail',
+        },
+      },
+      {
+        $unwind: '$transactionDetail',
+      },
+      {
+        $group: {
+          _id: null,
+          totalAmount: { $sum: '$amount' },
+          totalNeeds: { $sum: '$transactionDetail.needs' },
+          totalWants: { $sum: '$transactionDetail.wants' },
+          totalInvest: { $sum: '$transactionDetail.invest' },
+        },
+      },
+    ]).exec();
+
     return {
-      totalAmount: 3_000_000,
-      totalInvest: 450_000,
-      totalNeeds: 500_000,
-      totalWants: 2_050_000,
+      totalAmount: result[0].totalAmount,
+      totalInvest: result[0].totalInvest,
+      totalNeeds: result[0].totalNeeds,
+      totalWants: result[0].totalWants,
     };
   }
 }
